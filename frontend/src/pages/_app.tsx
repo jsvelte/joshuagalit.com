29ce0357ae3dea44a43384ca7f6a1d3cd17237af
Joshua Galit <108642414+jsvelte@users.noreply.github.com>
2023-10-27 15:54:46 +0800

    JG-17 - [FE] Implement Chatbot with Messenger (#17)
    Co-authored-by: acatzk <joshuaimalay@gmail.com>

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,6 +5,7 @@ import React, { FC, useEffect } from 'react'
 import '~/styles/globals.css'
 import { cn } from '~/utils/cn'
 import { openSans } from '~/utils/font'
+import FacebbokMessenger from '~/lib/facebook-messenger'
 import { ThemeProvider } from '~/components/templates/theme-provider'
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }): JSX.Element => {
@@ -20,6 +21,7 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }): JSX.Element => {
       <div className={cn(openSans.className)}>
         <Component {...pageProps} />
       </div>
+      {process.env.NODE_ENV === 'production' && <FacebbokMessenger />}
     </ThemeProvider>
   )
 }
