aff5c7d64436492dde2c28f4e4839633b83d4f52
acatzk <joshuaimalay@gmail.com>
2023-10-31 19:59:31 +0800

    JG-19 - [FE] Add New Testimony with Sir Nilo

--- frontend/src/utils/constant/testimonials.ts
+++ frontend/src/utils/constant/testimonials.ts
@@ -16,7 +16,7 @@ export const testimonials: Testimony[] = [
       <br />
       In summary, Joshua is a highly skilled and dedicated software developer with a wealth of experience in full stack development. His expertise in NextJS, Tailwindcss, Prisma, GraphQL and NestJS is invaluable to any project, and his professionalism and teamwork make him a true asset to any software development team. I feel fortunate to have had the opportunity to work alongside him, and I have no doubt that he will continue to excel in his career.
     `,
-    position: 'Fullstack/Mobile Developer At Halcyon Agine Company',
+    position: 'Fullstack/Mobile Developer at Halcyon Agine Company',
     companyImageUrl: '/images/company/halcyon.png'
   },
   {
@@ -41,6 +41,18 @@ export const testimonials: Testimony[] = [
       I wholeheartedly recommend him for your web development and business digitalization needs.
     `,
     position: 'Fullstack Developer at Sun Asterisk Philippines',
+    companyImageUrl: '/images/company/sun-asterisk.png'
+  },
+  {
+    profileImageUrl: '/images/testimonials/nilo-castillano-jr.png',
+    name: 'Nilo Castillano Jr.',
+    text: `
+      It has been a pleasure working alongside Joshua for more than a year, during which I've had the privilege of observing his outstanding proficiency in frontend development. Joshua stands out for his ability to build strong relationships with colleagues, his approachable demeanor, and his consistent contributions of valuable insights during our scrum meetings. He is unquestionably a dedicated team player, and the excellence of his work underscores his profound expertise across a wide range of technology stacks.
+      <br />
+      <br />
+      I wholeheartedly recommend Joshua, and I am confident that he will prove to be a valuable addition to your company as well.
+    `,
+    position: 'Web Developer at Sun Asterisk Philippines',
     companyImageUrl: '/images/company/sun-asterisk.png'
   }
 ]
