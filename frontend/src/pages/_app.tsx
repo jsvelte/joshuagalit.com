bcc006e8f5de21c08bc817d848100ebd42c414d3
acatzk <joshuaimalay@gmail.com>
2024-02-13 11:32:25 +0800

    JG-29 - [FE] Replace Messenger Chatbot to Crisp Chatbot

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,8 +2,6 @@ import Aos from 'aos'
 import React, { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { Toaster } from 'react-hot-toast'
-// @ts-expect-error: Temporary workaround for missing types in 'react-messenger-customer-chat' library
-import MessengerCustomerChat from 'react-messenger-customer-chat'
 
 import '~/styles/globals.css'
 import { openSans } from '~/utils/font'
@@ -22,12 +20,6 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <Toaster position="bottom-center" />
         <Component {...pageProps} />
-        {process.env.NODE_ENV === 'production' && (
-          <MessengerCustomerChat
-            pageId={process.env.MESSENGER_PAGE_ID}
-            appId={process.env.MESSENGER_APP_ID}
-          />
-        )}
       </ThemeProvider>
     </div>
   )
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -8,6 +8,21 @@ export default function Document(): JSX.Element {
         <meta charSet="utf-8" />
         <link rel="icon" href="/logo.png" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.$crisp=[];
+              window.CRISP_WEBSITE_ID="68034a31-3fb5-40ad-ae1e-3450b75e3e42";
+              (function(){
+                var d=document;
+                var s=d.createElement("script");
+                s.src="https://client.crisp.chat/l.js";
+                s.async=1;
+                d.getElementsByTagName("head")[0].appendChild(s);
+              })();
+              `
+          }}
+        />
       </Head>
       <body>
         <Main />
